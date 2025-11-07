e9e6e5a3c26d3ad1ba3b1aee95e7987a6d548b67
Ahmed-Ziaa <ahmedzia51345@gmail.com>
2025-11-07 16:20:05 +0500

    few changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolio')
+mongoose.connect('process.env.MONGODB_URI')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
